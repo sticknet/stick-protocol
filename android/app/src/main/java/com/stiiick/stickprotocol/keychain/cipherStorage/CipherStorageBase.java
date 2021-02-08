a949838ce077cf1324b1bf328a8a97e43bba2205
Omar Basem <founder@stiiick.com>
2021-02-08 08:47:06 +0400

    updates

--- android/app/src/main/java/com/stiiick/stickprotocol/keychain/cipherStorage/CipherStorageBase.java
+++ android/app/src/main/java/com/stiiick/stickprotocol/keychain/cipherStorage/CipherStorageBase.java
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c) 2020-2021 STiiiCK.
+ *
+ *  This source code is licensed under the GPLv3 license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
+ */
+
 package com.stiiick.stickprotocol.keychain.cipherStorage;
 
 import android.os.Build;
